be0fd574829e264864222f9013f05ba041a28d60
Rahul158625 <rahul.23cs110@sode-edu.in>
2024-09-21 13:25:24 +0530

    TestEmployee.java

--- TestEmployee.java
+++ TestEmployee.java
@@ -20,7 +20,6 @@ public class TestEmployee{
   public static void main(String[] args){
     Employee emp1=new Employee(1,"Rahul",5000);
     emp1.getEmpDetails();
-    
     emp1.raiseSalary(20);
     emp1.getEmpDetails();
   
